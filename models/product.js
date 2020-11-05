2c5f44cdd55acde6e637e28c4b66b277ba7b9cb0
rez <benjaminresnicoff@icloud.com>
2020-11-05 14:16:46 -0600

    completed models code

---
+++ models/product.js
@@ -0,0 +1,38 @@
+
+module.exports = function(sequelize, DataTypes) {
+  var Product = sequelize.define("Product", {
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    edible: {
+      type: DataTypes.BOOLEAN
+    },
+    price: {
+        type: DataTypes.NUMBER,
+        allowNull: false
+    },
+    currency: {
+        type: DataTypes.STRING,
+        allowNull: false
+
+    },
+    quantity: {
+        type: DataTypes.STRING,
+        allowNull: false
+
+    },
+    payment_method: {
+        type: DataTypes.STRING,
+        allowNull: false
+
+    },
+    item_type: {
+        type: DataTypes.STRING,
+        allowNull: false
+
+    },
+    });
+  
+  return Product;
+};
